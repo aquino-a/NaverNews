d38e406a36f17b9ea1b708f8e0be1282cf6c24dc
Alex <29205589+aquino-a@users.noreply.github.com>
2023-08-14 10:52:38 +0900

    add default user

--- NaverNews.Web/Data/ApplicationDbContext.cs
+++ NaverNews.Web/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NaverNews.Web.Models;
@@ -12,6 +13,31 @@ namespace NaverNews.Web.Data
             : base(options, operationalStoreOptions)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder
+                .Entity<IdentityUser>()
+                .HasData(
+                    new IdentityUser
+                    {
+                        Id = "04fc2e85-06d3-4be1-89fa-a87579465e85",
+                        UserName = "aquino@beancorp.com",
+                        NormalizedUserName = "AQUINO@BEANCORP.COM",
+                        Email = "aquino@beancorp.com",
+                        NormalizedEmail = "AQUINO@BEANCORP.COM",
+                        EmailConfirmed = true,
+                        PasswordHash = "AQAAAAIAAYagAAAAEMLjSL5I8JiyQD0peAP1n8FGncPCFZT71fuQni6DDR/bH7Lv7iZVsY2CcOjq69HY3Q==",
+                        SecurityStamp = "NAJOOAIGEZII4EMEAOLSGJYEIWLTJG6",
+                        ConcurrencyStamp = "49f14206-464f-4f8e-9690-a78c9a07995b",
+                        PhoneNumber = null,
+                        PhoneNumberConfirmed = false,
+                        TwoFactorEnabled = false,
+                        LockoutEnabled = false,
+                        AccessFailedCount = 0
+                    });
         }
     }
 }
--- NaverNews.Web/Data/Migrations/20230814015223_DefaultUser.Designer.cs
+++ NaverNews.Web/Data/Migrations/20230814015223_DefaultUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NaverNews.Web.Data;
 
@@ -10,9 +11,11 @@ using NaverNews.Web.Data;
 namespace NaverNews.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230814015223_DefaultUser")]
+    partial class DefaultUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
