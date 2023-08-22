806256dcfbc7c691de139c1e01b15b9ae64a522e
Alex <29205589+aquino-a@users.noreply.github.com>
2023-08-22 12:48:01 +0900

    add id to tokens

--- NaverNews.Core/Twitter/Tokens.cs
+++ NaverNews.Core/Twitter/Tokens.cs
@@ -1,16 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace NaverNews.Core
+﻿namespace NaverNews.Core
 {
-    [Keyless]
     public class Tokens
     {
-        public string Refresh { get; set; }
         public string Access { get; set; }
 
         /// <summary>
         /// Uses UTC.
         /// </summary>
         public DateTime ExpireTime { get; internal set; }
+
+        public string Refresh { get; set; }
+        public string TokensId { get; set; }
     }
 }
