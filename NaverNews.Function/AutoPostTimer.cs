1155ddb809d6796f3b5b1c2766ac7211f0b1de99
Alex <29205589+aquino-a@users.noreply.github.com>
2023-08-23 11:09:03 +0900

    rewrite to be isolated

--- NaverNews.Function/AutoPostTimer.cs
+++ NaverNews.Function/AutoPostTimer.cs
@@ -1,33 +1,32 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using NaverNews.Core;
 
 namespace NaverNews.Function
 {
-    public class AutoPostTimer
-    {
-        private readonly IArticleService _articleService;
+	public class AutoPostTimer
+	{
+		private readonly IArticleService _articleService;
 
-        public AutoPostTimer(IArticleService articleService)
-        {
-            _articleService = articleService;
-        }
+		public AutoPostTimer(IArticleService articleService)
+		{
+			_articleService = articleService;
+		}
 
-        [FunctionName("AutoPostTimer")]
-        public async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger logger)
-        {
-            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            try
-            {
-                await _articleService.AutoPost();
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "Problem autoposting articles.");
-            }
-        }
-    }
+		[Function("AutoPostTimer")]
+		public async Task Run([TimerTrigger("0 */1 * * * *")] ILogger logger)
+		{
+			logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+			try
+			{
+				await _articleService.AutoPost();
+			}
+			catch (Exception e)
+			{
+				logger.LogError(e, "Problem autoposting articles.");
+			}
+		}
+	}
 }
---
+++ NaverNews.Function/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NaverNews.Core;
+
+var connectionString = Environment.GetEnvironmentVariable("Cosmos-Connection") ?? throw new InvalidOperationException("Connection string 'Cosmos-Connection' not found.");
+var chatGptApiKey = Environment.GetEnvironmentVariable("ChatGpt:apiKey") ?? throw new InvalidOperationException("ChatGpt Api key 'ChatGpt:apiKey' not found.");
+var twitterClientId = Environment.GetEnvironmentVariable("Twitter:clientId") ?? throw new InvalidOperationException("Twitter client id 'Twitter:clientId' not found.");
+var twitterClientSecret = Environment.GetEnvironmentVariable("Twitter:clientSecret") ?? throw new InvalidOperationException("Twitter client secret 'Twitter:clientSecret' not found.");
+
+var engagementMinimum = Convert.ToInt32(Environment.GetEnvironmentVariable("Article:engagementMinimum"));
+var searchPageCount = Convert.ToInt32(Environment.GetEnvironmentVariable("Article:searchPageCount"));
+var skipThreshhold = Convert.ToInt32(Environment.GetEnvironmentVariable("Article:skipThreshhold"));
+
+var builder = new HostBuilder()
+	.ConfigureFunctionsWorkerDefaults()
+	.ConfigureServices(s =>
+	{
+		s.AddScoped<IArticleService, ArticleService>(sp =>
+		{
+			var articleService = new ArticleService(
+				sp.GetRequiredService<NaverClient>(),
+				sp.GetRequiredService<IChatGptService>(),
+				sp.GetRequiredService<TwitterClient>(),
+				sp.GetRequiredService<ArticleDbContext>(),
+				sp.GetRequiredService<ILogger<ArticleService>>());
+
+			articleService.EngagementMinimum = engagementMinimum;
+			articleService.SearchPageCount = searchPageCount;
+			articleService.SkipThreshhold = skipThreshhold;
+
+			return articleService;
+		});
+
+		s.AddDbContext<ArticleDbContext>(options => options.UseCosmos(connectionString, "news"), ServiceLifetime.Scoped);
+		s.AddDbContext<TokenDbContext>(options => options.UseCosmos(connectionString, "news"), ServiceLifetime.Transient, ServiceLifetime.Transient);
+		s.AddTransient<HttpClient>();
+		s.AddScoped<NaverClient>();
+		s.AddScoped<IChatGptService, ChatGptService>(sp => new ChatGptService(sp.GetRequiredService<HttpClient>(), chatGptApiKey));
+		s.AddSingleton<TokenService>(sp =>
+		{
+			var tokenService = new TokenService(twitterClientId,
+												twitterClientSecret,
+												sp.GetRequiredService<TokenDbContext>(),
+												sp.GetRequiredService<HttpClient>(),
+												sp.GetRequiredService<ILogger<TokenService>>());
+			tokenService.Load().GetAwaiter().GetResult();
+
+			return tokenService;
+		});
+
+		s.AddScoped<TwitterClient>((sp) =>
+		{
+			var tc = new TwitterClient(sp.GetRequiredService<TokenService>(),
+									   sp.GetRequiredService<HttpClient>(),
+									   sp.GetRequiredService<ILogger<TwitterClient>>());
+			return tc;
+		});
+	});
+
+builder.Build().Run();
